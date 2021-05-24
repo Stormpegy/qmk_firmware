35dabd1b4a9eedf861a5703d4a6b38230000e02d
Stormpegy <stormpegy@maxabears.com>
2021-05-24 23:42:11 +1000

    commit pls

--- keyboards/stormpegy/pain273/pain273.h
+++ keyboards/stormpegy/pain273/pain273.h
@@ -18,13 +18,13 @@
 
 #include "quantum.h"
 
-#define LAYOUT_all(\
+#define LAYOUT_all( \
     L00, L01, L02, L03, L04, L05, L06, L07, L08, L09, \
     L10, L11, L12, L13, L14, L15, L16, L17, L18,      \
-         L21, L22, L23, L24, L25, L26, L27, L28,      \
+         L21, L22, L23, L24, L25, L26, L27, L28       \
 ) \
-    { \
+{ \
     { L00, L01, L02, L03, L04, L05, L06, L07, L08, L09 }, \
     { L10, L11, L12, L13, L14, L15, L16, L17, L18      }, \
-    {      L21, L22, L23, L24, L25, L26, L27, L28      }  \
+    {      L21, L22, L23, L24, L25, L26, L27, L28      }, \
 }
